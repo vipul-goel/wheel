6700ecdbb8b611044ded9c4fb4268238e6deb103
Vipul Goel <vipulgoel04@gmail.com>
2021-06-29 14:11:51 +0530

    Completed edit note page UI

--- app/javascript/src/components/Dashboard/Notes/NewNotePane.jsx
+++ app/javascript/src/components/Dashboard/Notes/NewNotePane.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Pane } from "neetoui";
 import NewNoteForm from "./NewNoteForm";
+import { INITIAL_NOTE_FORM_VALUES } from "./constants";
 
-export default function NewNotePane({ addNewNote, showPane, setShowPane }) {
-  const onClose = () => setShowPane(false);
+export default function NewNotePane({
+  addNewNote,
+  updateNote,
+  isEdit,
+  noteData,
+  showPane,
+  onClose,
+}) {
   return (
     <Pane title="Add Note" isOpen={showPane} onClose={onClose}>
       <div className="px-6">
-        <NewNoteForm onClose={onClose} addNewNote={addNewNote} />
+        <NewNoteForm
+          onClose={onClose}
+          saveChanges={isEdit ? updateNote : addNewNote}
+          initialFormValues={isEdit ? noteData : INITIAL_NOTE_FORM_VALUES}
+        />
       </div>
     </Pane>
   );
